54a200b0f84a7b2152382a2c9a27b34b1a98c346
Bryan Neo <bryanncy1999@gmail.com>
2018-03-21 11:16:27 +0000

    Added page to update changes from github repo

---
+++ update.php
@@ -0,0 +1,4 @@
+<?php
+$output = "<pre>".shell_exec("/home/ubuntu/UCD_Assignment_Swinburne_Feb18_T1G3/cgi-bin/update.sh")."</pre>";
+echo $output;
+?>
---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Update changes from Github repo"
+echo ""
+echo Directory: $(pwd)
+echo "Running: git pull origin"
+echo ""
+echo $(git pull origin)
