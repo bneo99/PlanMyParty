2d39b4cd54febbd2421c938a8119243af9bea6c6
Bryan Neo <bryanncy1999@gmail.com>
2018-03-21 11:24:02 +0000

    fix some stuff with repo update page

--- update.php
+++
@@ -1,4 +0,0 @@
-<?php
-$output = "<pre>".shell_exec("/home/ubuntu/UCD_Assignment_Swinburne_Feb18_T1G3/cgi-bin/update.sh")."</pre>";
-echo $output;
-?>
---
+++ update/update.php
@@ -0,0 +1,4 @@
+<?php
+$output = "<pre>".shell_exec("/home/ubuntu/UCD_Assignment_Swinburne_Feb18_T1G3/update/update.sh")."</pre>";
+echo $output;
+?>
