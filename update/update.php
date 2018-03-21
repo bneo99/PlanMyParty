674f6dca7356b45f81b69c397981da2227c7d1ed
Bryan Neo <bryanncy1999@gmail.com>
2018-03-21 12:57:26 +0000

    fix github update scripts

--- update/update.php
+++ update/update.php
@@ -1,4 +1,7 @@
 <?php
+$oldldpath = getenv('LD_LIBRARY_PATH');
+putenv("LD_LIBRARY_PATH=");
 $output = "<pre>".shell_exec("/home/ubuntu/UCD_Assignment_Swinburne_Feb18_T1G3/update/update.sh")."</pre>";
+putenv("LD_LIBRARY_PATH=$oldldpath");
 echo $output;
 ?>
--- update/update.sh
+++ update/update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Update changes from Github repo"
 echo ""
+echo $(cd ..)
 echo Directory: $(pwd)
 echo "Running: git pull origin"
 echo ""
-echo $(git pull origin)
+echo $(git pull origin 2>&1)
+echo ""
+echo "Update complete."
