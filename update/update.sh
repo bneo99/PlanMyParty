32743dfa867dbe3a39f1cd61e4db1ccc5ff0dd3d
Bryan Neo <bryanncy1999@gmail.com>
2018-03-21 21:03:41 +0800

    update update.sh

--- update/update.sh
+++ update/update.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "Update changes from Github repo"
 echo ""
-echo $(cd ..)
-echo Directory: $(pwd)
 echo "Running: git pull origin"
 echo ""
 echo $(git pull origin 2>&1)
 echo ""
 echo "Update complete."
+echo Directory: $(pwd)
