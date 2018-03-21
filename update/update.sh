54bf97ad4f9d6aa2569d84a4258314c2dc8bd1fe
Bryan Neo <bryanncy1999@gmail.com>
2018-03-21 21:06:49 +0800

    more update.sh updates
    very unprofessional ik ik

--- update/update.sh
+++ update/update.sh
@@ -3,7 +3,4 @@ echo "Update changes from Github repo"
 echo ""
 echo "Running: git pull origin"
 echo ""
-echo $(git pull origin 2>&1)
-echo ""
-echo "Update complete."
-echo Directory: $(pwd)
+echo "$(git pull origin 2>&1)" && echo "" &&echo "Update complete."
