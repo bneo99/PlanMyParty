ebfa71237aebaeda1575d86261a777ec198f8196
Bryan Neo <bryanncy1999@gmail.com>
2018-04-01 23:01:07 +0800

    Remove w3-bottom for footer
    Havint w3-bottom looks nicer on short pages by forcing the footer to be on the bottom but this causes the footer to persist on the viewport therefore blocking significant screen area especially on small screens

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,5 @@
 		<!-- Footer -->
-		<footer class="w3-container w3-theme-dark w3-bottom">
+		<footer class="w3-container w3-theme-dark">
 			<p>&copy; Plan My Party 2018</p>
 			<p>Last updated: WhatDay, dd-mm-yy at hh:mm am/pm  (get a script to read modification time perhaps)</p>
 			<p>Maintained by: Webmaster (webmaster@planmyparty.ofcoursethisisfakedotcom)</p>
