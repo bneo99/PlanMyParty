ba5f0da5f03fcae137dad7c85e3bb23e782e8fd1
Bryan Neo <bryanncy1999@gmail.com>
2018-04-03 12:23:28 +0800

    remove unused code, change scroll pixel before showing to top button

--- template/script.php
+++ template/script.php
@@ -16,22 +16,11 @@
 		var toTopButton = document.getElementById('totopbutton');
 
 
-		if ($(window).scrollTop() == 0){
-			/* topBar.className = topBar.className.replace(" w3-top", "");
-			
-			if (userButton.className.indexOf("w3-show") == -1) {
-				userButton.className += " w3-show";
-			} */
+		if ($(window).scrollTop() <= 20){
 			
 			toTopButton.className = toTopButton.className.replace(" w3-show", "");
 		} else { 
-			/* if (topBar.className.indexOf("w3-top") == -1) {
-				topBar.className += " w3-top";
-			}
-			
-			userButton.className = userButton.className.replace(" w3-show", "");
-			userMenu.className = userMenu.className.replace(" w3-show", "");
-			 */
+
 			if (toTopButton.className.indexOf("w3-show") == -1) {
 				toTopButton.className += " w3-show";
 			}
