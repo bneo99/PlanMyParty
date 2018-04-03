0f1e87621c9f3d7b71b33a7ec6858c4a163efaf6
Bryan Neo <bryanncy1999@gmail.com>
2018-04-03 11:39:48 +0800

    Add Last Modified script to footer

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,12 @@
 		<!-- Footer -->
 		<footer class="w3-container w3-theme-dark">
 			<p>&copy; Plan My Party 2018</p>
-			<p>Last updated: WhatDay, dd-mm-yy at hh:mm am/pm  (get a script to read modification time perhaps)</p>
+			
+			<?php 
+				date_default_timezone_set("Asia/Kuching");
+				$mod_time = date (DateTime::RFC850, getlastmod()); 
+			?>
+			<p>Last updated: <?php echo $mod_time; ?></p>
 			<p>Maintained by: Webmaster (webmaster@planmyparty.ofcoursethisisfakedotcom)</p>
 		</footer>
 	</body>
