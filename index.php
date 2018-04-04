2d44b1f62d2f7af8d70449c0408b1530b00be6a5
Bryan Neo <bryanncy1999@gmail.com>
2018-04-04 09:39:01 +0800

    Change title back to navbar.php
    This removes the <title> tag from the body and uses JavaScript to change the title. The page now passes validation

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 ?>
 <!-- Set page title-->
-<title>Home | Plan My Party!</title>
+<script>
+	document.title = "Home | Plan My Party!";
+</script>
+
 <div class="w3-container">
 	<h1>Plan My Party</h1>
 	<p>testing</p>
 	<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 </div>
+
 <?php
 include $_SERVER["DOCUMENT_ROOT"].'/template/footer.php';
 include $_SERVER["DOCUMENT_ROOT"].'/template/script.php';
