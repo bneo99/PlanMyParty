37fb0910800d1304ef48f4b985e72d62e8ba8aa4
Bryan Neo <bryanncy1999@gmail.com>
2018-04-17 16:09:58 +0800

    Add Payment
    Removed Jason's html and merged the contents into /bookings/payment.php

---
+++ bookings/payment.php
@@ -0,0 +1,168 @@
+<?php
+include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
+?>
+<script>
+	/* modify this to change the title */
+	document.title = "Payment | Plan My Party!";
+// TODO: MAKE PAGE ONLY ACCESSIBLE WHEN LOGGED IN
+// but theres no need to make the form work. coz to make it work for real there would be too much effort that we might as well sell the site to someone looking to start a business
+</script>
+	
+<header class="w3-container w3-theme-d2">
+	<h1>Payment</h1>
+	<p style="text-align:center;">
+	In PMP, we accept a deposit of 50% prior to the event for first time customers and 30% for regular customers.</p>
+</header>
+
+<!-- this is supposed to read from database but we havent have the booking stuff setup so meh 
+TODO: 
+- read price from database
+- check for new customers (less than 5 parties)
+-->
+<div class="w3-container w3-padding-16 w3-theme-d4">
+	<h3>Total amount: RM 100.00</h3>
+	<h3>Deposit(50%): RM 50.00</h3>
+	<h2>To pay: RM 50.00</h2>
+	<p style="text-align:center;">
+	Please note that you have to pay the deposit in full before we can start processing your order.
+	</p>
+</div>
+
+<form name="payment_details" class="" method="post">
+<div class="w3-container w3-padding-16 w3-theme-l4">
+	<h4>Choose Payment Method</h4>
+	
+	<div class="w3-bar">
+	<button type="button" onClick="openPayWith('paypal')" class="w3-bar-item w3-button w3-theme-l3">
+		<img src="/images/paypal.png" alt="Paypal" class="paymentlogo w3-image"/>
+	</button>
+	
+	<button type="button" onClick="openPayWith('bitcoin')" class="w3-bar-item w3-button w3-theme-l3">
+		<img src="/images/bitcoin.png" alt="Bitcoin" class=" paymentlogo w3-image"/>
+	</button>
+	
+	<button type="button" onClick="openPayWith('creditcard')" class="w3-bar-item w3-button w3-theme-l3">
+		<img src="/images/visa.png" alt="Visa" class="paymentlogo w3-image"/>
+		<img src="/images/mastercard.png" alt="MasterCard" class="paymentlogo w3-image"/>
+	</button>
+	</div>
+	<br/>
+	
+	<div id="paypal" class=" paymentmethod" style="display:none;">
+		<label>Paypal Email:</label><input class="w3-input" type="text" name="paypal_email">
+		<p>
+		Note: When you click on Proceed, you will be redirected to Paypal's page to complete your payment.
+		</p>
+	</div>
+	
+	<div id="bitcoin" class=" paymentmethod" style="display:none;">
+		<label>Wallet Address:</label><input class="w3-input" type="text" name="bitcoin_addr">
+		<label>Transaction ID (txid):</label><input class="w3-input" type="text" name="bitcoin_txid">
+		<p>RM 50.00 = 0.0016BTC (17/4/18)</p>
+		<p>Pay to: 16wztd2HfKdzkxEg6jH8zh7FPZEcd7gpXW</p>
+		<p>Note: Please pay to the address above and enter your paying wallet address and the transaction id.</p>
+	</div>
+	
+	<div id="creditcard" class=" paymentmethod" style="display:none;">
+		<label>Name on card:</label><input class="w3-input" type="text" name="card_name">
+		<label>Card Number:</label><input class="w3-input" type="text" name="number">
+		<label>CVV2:</label><input class="w3-input" type="text" name="CW">
+		<label>Expiry Date:</label><input class="w3-input" type="text" name="expiry" placeholder="MM/YY">
+		<p>
+		Note: When you click on Proceed, you will be redirected to your bank's transaction page to complete your payment.
+		</p>
+	</div>
+</div>
+
+<div class="w3-container w3-padding-16 w3-theme-l2">
+	<h4>Biling details:</h4>
+	<p>
+	<label>Title:</label>
+	<select name="title" class="w3-input">
+		<option value="mr">Mr</option>
+		<option value="ms">Ms</option>
+		<option value="sir">Sir</option>
+		<option value="dr">Dr</option>
+		<option value="madam">Madam</option>
+		<option value="duke">Duke</option>
+		<option value="duchess">Duchess</option>
+		<option value="earl">Earl</option>
+		<option value="count">Count</option>
+		<option value="countess">Countess</option>
+		<option value="lord">Lord</option>
+		<option value="high_chancellor">High Chancellor</option>
+	</select>
+	</p><p>
+	<label>First name:</label><input type="text" name="first_name" class="w3-input">
+	</p><p>
+	<label>Last name:</label><input type="text" name="last_name" class="w3-input">
+	</p><p>
+	<label>Address:</label><input type="text" name="address" class="w3-input">
+	</p><p>
+	<label>City:</label><input type="text" name="city" class="w3-input">
+	</p><p>
+	<label>Postcode:</label><input type="text" name="postcode" class="w3-input">
+	</p><p>
+	<label>State:</label><select name="state" class="w3-input">
+		<option value="sarawak">Sarawak</option>
+		<option value="sabah">Sabah</option>
+		<option value="penang">Penang</option>
+		<option value="selangor">Selangor</option>
+		<option value="johor">Johor</option>
+		<option value="malacca">Malacca</option>
+		<option value="perak">Perak</option>
+		<option value="kedah">Kedah</option>
+		<option value="pahang">Pahang</option>
+		<option value="kelantan">Kelantan</option>
+		<option value="terengganu">Terengganu</option>
+		<option value="negeri_9">Negeri Sembilan</option>
+		<option value="perlis">Perlis</option>
+	</select>
+	</p><p>
+	<label>Mobile number:</label><input type="text" name="mobile_num" class="w3-input">
+	</p><p>
+	<label>Email address:</label><input type="email" name="email" class="w3-input">
+	</p><p>
+	<label>Confirm email address:</label><input type="email" name="confirm_email" class="w3-input">
+	</p><p>
+	<input type="checkbox" name="informed" checked="checked" class="w3-check"><label> Yes, I want to receive announcements and promotions from PMP</label>
+	</p>
+</div>
+
+<div class="w3-container w3-padding-16 w3-theme-l4">
+	<h2>Terms & Conditions</h2>
+	<p style="text-align:justify;">PMP is not responsible for any damage that may occur during the party planning process. This also includes the time when the party is being conducted. Any claim financially towards PMP will be rejected immediately without reason. If attempt to threaten, PMP will attempt to sue the prosecutor without hesitation. Your understanding is highly appreciated.</p>
+	<input type="checkbox" name="tnc" class="w3-check"><label> Yes, I have read the terms and conditions.</label>
+
+
+	<p>
+	<input type="submit" value="Proceed" class="w3-button w3-theme-d3">
+	<input type="button" value="Cancel booking" class="w3-button w3-theme-l2">
+	</p>
+</div>
+</form>
+<script>
+
+function openPayWith(paymentMethod){
+	var i;
+	var element = document.getElementById(paymentMethod);
+	var x = document.getElementsByClassName("paymentmethod");
+	
+	for (i=0;i<x.length;i++){
+		 x[i].style.display = "none"; 
+	}
+	element.style.display = "block";
+}
+
+</script>
+
+<style>
+.paymentlogo {
+	height:25px;
+}
+</style>
+
+<?php
+include $_SERVER["DOCUMENT_ROOT"].'/template/footer.php';
+include $_SERVER["DOCUMENT_ROOT"].'/template/script.php';
+?>
--- index.php
+++ index.php
@@ -7,137 +7,136 @@ include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 	
 </script>
 
-	<div id="header" class="w3-container w3-theme-d3">
-		<img src="images/logo.png" alt="Company Logo" class="w3-image">
-		
-		<h1>Plan My Party</h1>
-		
-		<div class="options">
-			<p>Our Packages</p>
-			<p>Customise Events</p>
+<div id="header" class="w3-container w3-theme-d3">
+	<img src="images/logo.png" alt="Company Logo" class="w3-image">
+	
+	<h1>Plan My Party</h1>
+	
+	<div class="options">
+		<p>Our Packages</p>
+		<p>Customise Events</p>
+	</div>
+</div>
+
+<section id="review" class="w3-container w3-padding-16 w3-theme-l4" >
+<div class="w3-display-container" style="margin:auto; max-width:700px;">
+	<h2>Our happy customers</h2>
+	
+	<button class="w3-button w3-theme w3-display-topleft" onclick="changeSlide(-1)">&#10094;</button>
+	<button class="w3-button w3-theme w3-display-topright" onclick="changeSlide(+1)">&#10095;</button>
+	
+	<!-- todo: change slideshow stuff to directly replace the pics, reviews so the height doesnt change everything the pic has different aspect ratio 
+	
+	once the todo above is done the buttons can be put back to the left/right of the pic-->
+	<div class="w3-black" style="text-align:center;" >
+
+		<div class="w3-display-container mySlides w3-animate-opacity">
+			<img src="/images/review_1.jpg" class="w3-image slides">
+			<div class="w3-display-bottom w3-container w3-padding-16 w3-theme-dark">"Was a very special party for me. Definently a solid 4 out of 5."</div>
+		</div>
+
+		<div class="w3-display-container mySlides w3-animate-opacity">
+			<img src="/images/review_2.jpg" class="w3-image slides">
+			<div class="w3-display-bottom w3-container w3-padding-16 w3-theme-dark">"Had the best time of my life. Thank you PMP!!"</div>
+		</div>
+
+		<div class="w3-display-container mySlides w3-animate-opacity">
+			<img src="/images/review_3.jpg" class="w3-image slides">
+			<div class="w3-display-bottom w3-container w3-padding-16 w3-theme-dark">"My girls and I enjoyed ourselves that night. 5 out of 5 for me."</div>
 		</div>
 	</div>
-	
-	<section id="review" class="w3-container w3-padding-16 w3-theme-l4" >
-	<div class="w3-display-container" style="margin:auto; max-width:700px;">
-		<h2>Our happy customers</h2>
+</div>
+</section>
+
+<div class="w3-container w3-padding-16">
+	<h2>Too busy to customise?</h2>
+	<h3>Choose from our packages</h3>
 		
-		<button class="w3-button w3-theme w3-display-topleft" onclick="changeSlide(-1)">&#10094;</button>
-		<button class="w3-button w3-theme w3-display-topright" onclick="changeSlide(+1)">&#10095;</button>
-		
-		<!-- todo: change slideshow stuff to directly replace the pics, reviews so the height doesnt change everything the pic has different aspect ratio 
-		
-		once the todo above is done the buttons can be put back to the left/right of the pic-->
-		<div class="w3-black" style="text-align:center;" >
+		<span class="w3-button">
+		<img src="/images/pax_1.jpg" alt="Simple Plan Image" class="w3-image">
+		<p>Simple (10-15 pax)</p>
+		</span>
+		<span class="w3-button">
+		<img src="/images/pax_2.jpg" alt="Regular Plan Image" class="w3-image">
+		<p>Regular (16-10 pax)</p>
+		</span>
+		<span class="w3-button">
+		<img src="/images/pax_3.jpg" alt="Extravagant Plan Image" class="w3-image">
+		<p>Extravagant (>30 pax)</p>
+		</span>
+</div>
 
-			<div class="w3-display-container mySlides w3-animate-opacity">
-				<img src="images/review_1.jpg" class="w3-image slides">
-				<div class="w3-display-bottom w3-container w3-padding-16 w3-theme-dark">"Was a very special party for me. Definently a solid 4 out of 5."</div>
-			</div>
-
-			<div class="w3-display-container mySlides w3-animate-opacity">
-				<img src="images/review_2.jpg" class="w3-image slides">
-				<div class="w3-display-bottom w3-container w3-padding-16 w3-theme-dark">"Had the best time of my life. Thank you PMP!!"</div>
-			</div>
-
-			<div class="w3-display-container mySlides w3-animate-opacity">
-				<img src="images/review_3.jpg" class="w3-image slides">
-				<div class="w3-display-bottom w3-container w3-padding-16 w3-theme-dark">"My girls and I enjoyed ourselves that night. 5 out of 5 for me."</div>
-			</div>
-		</div>
-	</div>
-	</section>
+<div class="w3-container w3-theme-l4">
+	<h2>Our partners</h2>
+	<h3>The backbone of our services</h3>
 	
-	<div class="w3-container w3-padding-16">
-		<h2>Too busy to customise?</h2>
-		<h3>Choose from our packages</h3>
-			
-			<span class="w3-button">
+	<img src="/images/mobydick.png" alt="Logo of third party company" class="w3-image sponsorlogo">
+	<img src="/images/foursquares.png" alt="Logo of third party company" class="w3-image sponsorlogo">
+	<img src="/images/gentinghighlands.png" alt="Logo of third party company" class="w3-image sponsorlogo">
+	<img src="/images/redridinghood.png" alt="Logo of third party company" class="w3-image sponsorlogo">
+	<img src="/images/toysrus.png" alt="Logo of third party company" class="w3-image sponsorlogo">
+	<img src="/images/rotiboy.png" alt="Logo of third party company" class="w3-image sponsorlogo">
 
-			<img src="images/pax_1.jpg" alt="Simple Plan Image" class="w3-image">
-			<p>Simple (10-15 pax)</p>
-			</span>
-			<span class="w3-button">
-			<img src="images/pax_2.jpg" alt="Regular Plan Image" class="w3-image">
-			<p>Regular (16-10 pax)</p>
-			</span>
-			<span class="w3-button">
-			<img src="images/pax_3.jpg" alt="Extravagant Plan Image" class="w3-image">
-			<p>Extravagant (>30 pax)</p>
-			</span>
-	</div>
-	
-	<div class="w3-container w3-theme-l4">
-		<h2>Our partners</h2>
-		<h3>The backbone of our services</h3>
-		
-		<img src="images/mobydick.png" alt="Logo of third party company" class="w3-image sponsorlogo">
-		<img src="images/foursquares.png" alt="Logo of third party company" class="w3-image sponsorlogo">
-		<img src="images/gentinghighlands.png" alt="Logo of third party company" class="w3-image sponsorlogo">
-		<img src="images/redridinghood.png" alt="Logo of third party company" class="w3-image sponsorlogo">
-		<img src="images/toysrus.png" alt="Logo of third party company" class="w3-image sponsorlogo">
-		<img src="images/rotiboy.png" alt="Logo of third party company" class="w3-image sponsorlogo">
+	<img src="/images/chickenriceshop.png" alt="Logo of third party company" class="w3-image sponsorlogo">
+	<img src="/images/pepsi.png" alt="Logo of third party company" class="w3-image sponsorlogo">
+	<img src="/images/sunway.png" alt="Logo of third party company" class="w3-image sponsorlogo">
+	<img src="/images/cocacola.png" alt="Logo of third party company" class="w3-image sponsorlogo sponsorlogo">
+	<img src="/images/jamiesequipments.png" alt="Logo of third party company" class="w3-image sponsorlogo">
 
-		<img src="images/chickenriceshop.png" alt="Logo of third party company" class="w3-image sponsorlogo">
-		<img src="images/pepsi.png" alt="Logo of third party company" class="w3-image sponsorlogo">
-		<img src="images/sunway.png" alt="Logo of third party company" class="w3-image sponsorlogo">
-		<img src="images/cocacola.png" alt="Logo of third party company" class="w3-image sponsorlogo sponsorlogo">
-		<img src="images/jamiesequipments.png" alt="Logo of third party company" class="w3-image sponsorlogo">
+</div>
 
-	</div>
-	
-	<script>
-	var slideIndex = 0;
-	showSlides(slideIndex);
+<script>
+var slideIndex = 0;
+showSlides(slideIndex);
 
-	function changeSlide(n){
-		showSlides(slideIndex += n);
+function changeSlide(n){
+	showSlides(slideIndex += n);
+}
+
+function nextSlide(){
+	showSlides(slideIndex += 1);
+}
+
+function showSlides(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	if (n > x.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = x.length} ;
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
 	}
+	x[slideIndex-1].style.display = "block"; 
 	
-	function nextSlide(){
-		showSlides(slideIndex += 1);
-	}
-	
-	function showSlides(n) {
-		var i;
-		var x = document.getElementsByClassName("mySlides");
-		if (n > x.length) {slideIndex = 1}
-		if (n < 1) {slideIndex = x.length} ;
-		for (i = 0; i < x.length; i++) {
-			x[i].style.display = "none";
-		}
-		x[slideIndex-1].style.display = "block"; 
-		
-	}
-	
-	/* wait for 4 seconds  */
-	setInterval(nextSlide, 4000);
-	
-	</script>
-	
-	<style> 
-	/*
-	do styles in the file directly coz its only on this page 
-	only add to /styles/style.css if its a global css (applies to all/most pages)
-	*/
-	.partners img{
-		width: 200px;
-		float: left;
-		margin: 3% 3.2%;
-	}
-	
-	.sponsorlogo{
-		height:100px;
-		margin:10px;
-		text-align:center;
-	}
-	
-	.slides{
-		max-height:300px;
-		width:auto;
-	}
+}
 
-	</style>
+/* wait for 4 seconds  */
+setInterval(nextSlide, 4000);
+
+</script>
+
+<style> 
+/*
+do styles in the file directly coz its only on this page 
+only add to /styles/style.css if its a global css (applies to all/most pages)
+*/
+.partners img{
+	width: 200px;
+	float: left;
+	margin: 3% 3.2%;
+}
+
+.sponsorlogo{
+	height:100px;
+	margin:10px;
+	text-align:center;
+}
+
+.slides{
+	max-height:300px;
+	width:auto;
+}
+
+</style>
 <?php
 include $_SERVER["DOCUMENT_ROOT"].'/template/footer.php';
 include $_SERVER["DOCUMENT_ROOT"].'/template/script.php';
