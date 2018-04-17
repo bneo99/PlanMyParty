bc4391f174a67bde75171501bdbeb632c24b95dd
Bryan Neo <bryanncy1999@gmail.com>
2018-04-17 11:48:25 +0800

    Update index.php CSS
    Removed index_style.css and merged css thats still being used into index.php itself.

--- index.php
+++ index.php
@@ -5,15 +5,6 @@ include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 <script>
 	document.title = "Home | Plan My Party!";
 	
-	var head = document.getElementsByTagName('head')[0];
-	var pageCss = document.createElement('link');
-	pageCss.id = 'pagecss';
-	pageCss.rel = 'stylesheet';
-	pageCss.type = 'text/css';
-	pageCss.href = '/styles/index_style.css';
-	pageCss.media = 'all';
-	head.appendChild(pageCss);
-	
 </script>
 
 	<div id="header" class="w3-container w3-theme-d3">
@@ -28,7 +19,7 @@ include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 	</div>
 	
 	<section id="review" class="w3-container w3-padding-16 w3-theme-l4" >
-	<div id="slideshow-container" class="w3-display-container reviews" style="margin:auto; max-width:700px;">
+	<div class="w3-display-container" style="margin:auto; max-width:700px;">
 		<h2>Our happy customers</h2>
 		
 		<button class="w3-button w3-theme w3-display-topleft" onclick="changeSlide(-1)">&#10094;</button>
@@ -37,19 +28,19 @@ include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 		<!-- todo: change slideshow stuff to directly replace the pics, reviews so the height doesnt change everything the pic has different aspect ratio 
 		
 		once the todo above is done the buttons can be put back to the left/right of the pic-->
-		<div class="slideshow-container w3-black">
+		<div class="w3-black" style="text-align:center;" >
 
-			<div class="w3-display-container mySlides fade">
+			<div class="w3-display-container mySlides w3-animate-opacity">
 				<img src="images/review_1.jpg" class="w3-image slides">
 				<div class="w3-display-bottom w3-container w3-padding-16 w3-theme-dark">"Was a very special party for me. Definently a solid 4 out of 5."</div>
 			</div>
 
-			<div class="w3-display-container mySlides fade">
+			<div class="w3-display-container mySlides w3-animate-opacity">
 				<img src="images/review_2.jpg" class="w3-image slides">
 				<div class="w3-display-bottom w3-container w3-padding-16 w3-theme-dark">"Had the best time of my life. Thank you PMP!!"</div>
 			</div>
 
-			<div class="w3-display-container mySlides fade">
+			<div class="w3-display-container mySlides w3-animate-opacity">
 				<img src="images/review_3.jpg" class="w3-image slides">
 				<div class="w3-display-bottom w3-container w3-padding-16 w3-theme-dark">"My girls and I enjoyed ourselves that night. 5 out of 5 for me."</div>
 			</div>
@@ -57,28 +48,28 @@ include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 	</div>
 	</section>
 	
-	<div class="w3-container w3-padding-16 pax_selection">
+	<div class="w3-container w3-padding-16">
 		<h2>Too busy to customise?</h2>
 		<h3>Choose from our packages</h3>
 			
-			<span class="w3-button pax_1">
+			<span class="w3-button">
 
 			<img src="images/pax_1.jpg" alt="Simple Plan Image" class="w3-image">
 			<p>Simple (10-15 pax)</p>
 			</span>
-			<span class="w3-button pax_2">
+			<span class="w3-button">
 			<img src="images/pax_2.jpg" alt="Regular Plan Image" class="w3-image">
 			<p>Regular (16-10 pax)</p>
 			</span>
-			<span class="w3-button pax_3">
+			<span class="w3-button">
 			<img src="images/pax_3.jpg" alt="Extravagant Plan Image" class="w3-image">
 			<p>Extravagant (>30 pax)</p>
 			</span>
 	</div>
 	
-	<div class="w3-container w3-theme-l4 partners">
+	<div class="w3-container w3-theme-l4">
 		<h2>Our partners</h2>
-		<p>The backbone of our services</p>
+		<h3>The backbone of our services</h3>
 		
 		<img src="images/mobydick.png" alt="Logo of third party company" class="w3-image sponsorlogo">
 		<img src="images/foursquares.png" alt="Logo of third party company" class="w3-image sponsorlogo">
@@ -86,7 +77,6 @@ include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 		<img src="images/redridinghood.png" alt="Logo of third party company" class="w3-image sponsorlogo">
 		<img src="images/toysrus.png" alt="Logo of third party company" class="w3-image sponsorlogo">
 		<img src="images/rotiboy.png" alt="Logo of third party company" class="w3-image sponsorlogo">
-		
 
 		<img src="images/chickenriceshop.png" alt="Logo of third party company" class="w3-image sponsorlogo">
 		<img src="images/pepsi.png" alt="Logo of third party company" class="w3-image sponsorlogo">
@@ -124,7 +114,30 @@ include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 	setInterval(nextSlide, 4000);
 	
 	</script>
+	
+	<style> 
+	/*
+	do styles in the file directly coz its only on this page 
+	only add to /styles/style.css if its a global css (applies to all/most pages)
+	*/
+	.partners img{
+		width: 200px;
+		float: left;
+		margin: 3% 3.2%;
+	}
+	
+	.sponsorlogo{
+		height:100px;
+		margin:10px;
+		text-align:center;
+	}
+	
+	.slides{
+		max-height:300px;
+		width:auto;
+	}
 
+	</style>
 <?php
 include $_SERVER["DOCUMENT_ROOT"].'/template/footer.php';
 include $_SERVER["DOCUMENT_ROOT"].'/template/script.php';
