bf7a37156b2046e0381a3fe4975cef75b4078422
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2018-04-18 12:28:23 +0800

    Final update on contact and about
    with additional css design

--- contactus/index.php
+++ contactus/index.php
@@ -12,40 +12,43 @@ include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 	pageCss.id = 'pagecss';
 	pageCss.rel = 'stylesheet';
 	pageCss.type = 'text/css';
-	pageCss.href = '/contact_style.css';
+	pageCss.href = '/styles.css';
 	pageCss.media = 'all';
 	head.appendChild(pageCss);
 	
 </script>
-
-<h1>Contact Us</h1>
+<article>
+	<div style="margin-left: 10%; margin-right: 10%;">
+	<h1 style="text-align:center;">Contact Us</h1>
 	
-	<hr>
-	
+	<div class="w3-panel w3-margin w3-theme-l3 w3-padding-16">
 	<h2>Hotline</h2>
 	<p>Call us with this number: <a href="tel:123456789">123456789</a></p>
 	<p>You can use this number too: <a href="tel:987654321">987654321</a></p>
+	</div>
 	
-	<hr>
-	
+	<div class="w3-panel w3-margin w3-theme-l3 w3-padding-16">
 	<h2>Our Branches</h2>
-	<p>Wisma Saberkas, 3, Jalan Tun Abang Haji Openg, 93150 Kuching, Sarawak</p>
-	<p>Kuala Lumpur City Centre, 50088 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur</p>
-	<p>Lot PT 1485 Jalan Padang Hiliran Chabang 3 Kuala Terengganu, 21000 Kuala Terengganu, Terengganu</p>
-	<p>BLOK 4B-40-68,, JALAN RUMBIA KOMPLEKS BUKIT JAMBUL, Kampung Seberang Paya, 11900 Bayan Lepas, Penang</p>
+	<p>Wisma Saberkas, 3, Jalan Tun Abang Haji Openg, 93150 Kuching, <strong>Sarawak</strong></p>
+	<p>Kuala Lumpur City Centre, 50088 Kuala Lumpur, <strong>Wilayah Persekutuan Kuala Lumpur</strong></p>
+	<p>Lot PT 1485 Jalan Padang Hiliran Chabang 3 Kuala Terengganu, 21000 Kuala Terengganu, <strong>Terengganu</strong></p>
+	<p>BLOK 4B-40-68,, JALAN RUMBIA KOMPLEKS BUKIT JAMBUL, Kampung Seberang Paya, 11900 Bayan Lepas, <strong>Penang</strong></p>
+	</div>
 	
-	<hr>
-	
+	<div class="w3-panel w3-margin w3-theme-l3 w3-padding-16">
 	<h2>Email</h2>
 	<p>Email us at <a href="mailto:thisfake@nottrue.com">thisfake@nottrue.com</a></p>
 	<p>You can also try <a href="mailto:whydoukeeptrying@sthap.com">whydoukeeptrying@sthap.com</a></p>
+	</div>
 	
-	<hr>
-	
+	<div class="w3-panel w3-margin w3-theme-l3 w3-padding-16">
 	<h2>Social platforms</h2>
 	<p><a href="https://www.facebook.com/" target="_blank">Facebook</a></p>
 	<p><a href="https://www.instagram.com/?hl=en" target="_blank">Instagram</p>
 	<p><a href="https://twitter.com/?lang=en" target="_blank">Twitter</p>
+	</div>
+	</div>
+</article>
 
 <?php
 include $_SERVER["DOCUMENT_ROOT"].'/template/footer.php';
