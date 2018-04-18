a401a50e151821b077e09f8db23c7130254e3910
Bryan Neo <bryanncy1999@gmail.com>
2018-04-18 09:16:21 +0800

    Move stuff to pricing folder
    got the wrong folder

--- pricing/index.php
+++ pricing/index.php
@@ -1,18 +1,22 @@
-<!DOCTYPE html>
-
-<html>
-
-<head>
-	<title>My Booking | Plan My Party</title>
-	<meta charset="utf-8">
-	<meta name="author" content="Lester Tang Jia Rui">
-	<meta name="description" content="Booking Of The Party">
-	<meta name="keywords" content="party, planning, booking">
- 
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-
-<body>
+<?php
+include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
+?>
+<!-- Change some head values-->
+<script>
+	/* modify this to change the title */
+	document.title = "Home | Plan My Party!";
+	
+	/* modify the href to link to whatever css you want */
+	var head = document.getElementsByTagName('head')[0];
+	var pageCss = document.createElement('link');
+	pageCss.id = 'pagecss';
+	pageCss.rel = 'stylesheet';
+	pageCss.type = 'text/css';
+	pageCss.href = '/index_style.css';
+	pageCss.media = 'all';
+	head.appendChild(pageCss);
+	
+</script>
 	<h1>Let's Start Customising !</h1>
 	
 	<hr>
@@ -242,8 +246,10 @@
 	
 </div>
 
-</body>
-</html>
+<?php
+include $_SERVER["DOCUMENT_ROOT"].'/template/footer.php';
+include $_SERVER["DOCUMENT_ROOT"].'/template/script.php';
+?>
 
 	
 	
