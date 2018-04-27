81f6a2d6bcfd6f58b2669565789c9b4c3f0ff8b8
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2018-04-27 16:07:45 +0800

    review initials
    ms choo requested them

--- reviews/index.php
+++ reviews/index.php
@@ -14,17 +14,17 @@ include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 	<article>
 		<div class="w3-panel w3-margin w3-theme-l3 w3-padding-16">
 			<img class="w3-image w3-mobile" style="max-width:500px;" src="/images/review_1.jpg" alt="First review picture"/>
-			<p style="text-align:justify;">This birthday party was a blast for me. PMP did absolutely everything wonderful. From the food to the decoration, and not to mention the fantastic DJ that they hired. My sister would also like to thank PMP for making her 21st birthday such a memorable one. The reason I gave PMP a 4 out of 5 rating is that even though the party was fantastic, the process of planning it was frustrating. There were so many stressful moments at the time. For example, the food selection process was a hassle as some of the selections were out of order, so I had to resort to other alternatives which took up a lot of time. Nevertheless, PMP was able to deliver a birthday party that was beyond my expectations. In simple words, I wish that they would improve their party-planning process in the future.</p>
+			<p style="text-align:justify;">This birthday party was a blast for me. PMP did absolutely everything wonderful. From the food to the decoration, and not to mention the fantastic DJ that they hired. My sister would also like to thank PMP for making her 21st birthday such a memorable one. The reason I gave PMP a 4 out of 5 rating is that even though the party was fantastic, the process of planning it was frustrating. There were so many stressful moments at the time. For example, the food selection process was a hassle as some of the selections were out of order, so I had to resort to other alternatives which took up a lot of time. Nevertheless, PMP was able to deliver a birthday party that was beyond my expectations. In simple words, I wish that they would improve their party-planning process in the future. ~J.P.</p>
 		</div>
 
 		<div class="w3-panel w3-margin w3-theme-l3 w3-padding-16">
 			<img class="w3-image w3-mobile" style="max-width:500px;" src="/images/review_2.jpg" alt="Second review picture"/>
-			<p>All thanks to PMP, the reunion party for my old classmates back in high school was amazing. The atmosphere was terrific. Everyone was having a good time. To be honest, what PMP managed to do was beyond my expectations. I guess the best was to summarise it is, THANK YOU PMP!!!!!</p>
+			<p>All thanks to PMP, the reunion party for my old classmates back in high school was amazing. The atmosphere was terrific. Everyone was having a good time. To be honest, what PMP managed to do was beyond my expectations. I guess the best was to summarise it is, THANK YOU PMP!!!!! ~N.A.</p>
 		</div>
 
 		<div class="w3-panel w3-margin w3-theme-l3 w3-padding-16">
 			<img class="w3-image w3-mobile" style="max-width:500px;"src="/images/review_3.jpg" alt="Third review picture"/>
-			<p>The girls and I had the time of our lives thanks to PMP. I never thought that they would pull it off as I thought that they wouldn't put much effort to my party as its just a get-together party. On the contrary, they paid attention to every detail that was mentioned during the party-planning phase. It is for this that I would give a solid 5 out of 5 for PMP.</p>
+			<p>The girls and I had the time of our lives thanks to PMP. I never thought that they would pull it off as I thought that they wouldn't put much effort to my party as its just a get-together party. On the contrary, they paid attention to every detail that was mentioned during the party-planning phase. It is for this that I would give a solid 5 out of 5 for PMP. ~S.I.</p>
 		</div>
 	
 	</article>
