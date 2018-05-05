9a52ce6b7f931ae79bd70056081dec8771f7cb99
Bryan Neo <15075271+bneo99@users.noreply.github.com>
2018-05-05 09:52:17 +0800

    Update google maps
    Map script requires initMap to be declared before execution of script. therefore moved to before the script is called

--- pricing/index.php
+++ pricing/index.php
@@ -123,6 +123,72 @@ include $_SERVER["DOCUMENT_ROOT"].'/template/navbar.php';
 				
 				<input id="searchInput" class="controls" type="text" placeholder="Search with Google Maps or enter your own venue address!">
 				<div id="map" class="w3-margin-bottom w3-margin-top"></div>
+				<script>
+				//map stuff
+				function initMap() {
+					var map = new google.maps.Map(document.getElementById('map'), {
+						center: {lat: 1.5535, lng: 110.3593},
+						zoom: 13
+					});
+					var input = document.getElementById('searchInput');
+					map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+					var autocomplete = new google.maps.places.Autocomplete(input);
+					autocomplete.bindTo('bounds', map);
+
+					var infowindow = new google.maps.InfoWindow();
+					var marker = new google.maps.Marker({
+						map: map,
+						anchorPoint: new google.maps.Point(0, -29)
+					});
+
+					autocomplete.addListener('place_changed', function() {
+						infowindow.close();
+						marker.setVisible(false);
+						var place = autocomplete.getPlace();
+						if (!place.geometry) {
+							window.alert("Autocomplete's returned place contains no geometry");
+							return;
+						}
+
+						// If the place has a geometry, then present it on a map.
+						if (place.geometry.viewport) {
+							map.fitBounds(place.geometry.viewport);
+						} else {
+							map.setCenter(place.geometry.location);
+							map.setZoom(17);
+						}
+						marker.setIcon(({
+							url: place.icon,
+							size: new google.maps.Size(71, 71),
+							origin: new google.maps.Point(0, 0),
+							anchor: new google.maps.Point(17, 34),
+							scaledSize: new google.maps.Size(35, 35)
+						}));
+						marker.setPosition(place.geometry.location);
+						marker.setVisible(true);
+
+						var address = '';
+						if (place.address_components) {
+							address = [
+							(place.address_components[0] && place.address_components[0].short_name || ''),
+							(place.address_components[1] && place.address_components[1].short_name || ''),
+							(place.address_components[2] && place.address_components[2].short_name || '')
+							].join(' ');
+						}
+
+						infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+						infowindow.open(map, marker);
+					});
+
+					var myCenter = new google.maps.LatLng(1.5535, 110.3593);
+					var mapCanvas = document.getElementById("map");
+					var mapOptions = {center: myCenter, zoom: 13};
+					var map = new google.maps.Map(mapCanvas, mapOptions);
+					var marker = new google.maps.Marker({position:myCenter});
+					marker.setMap(map);
+				}
+				</script>
 				<!--the map-->
 				<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBi0GWwSbuuAbe6SLjrtZJF78p5_3EEQJ4&libraries=places&callback=initMap" async defer></script>
 			</div>
@@ -812,70 +878,6 @@ function showVenue(type) {
 	}
 }
 
-//map stuff
-function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 1.5535, lng: 110.3593},
-		zoom: 13
-	});
-	var input = document.getElementById('searchInput');
-	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-	var autocomplete = new google.maps.places.Autocomplete(input);
-	autocomplete.bindTo('bounds', map);
-
-	var infowindow = new google.maps.InfoWindow();
-	var marker = new google.maps.Marker({
-		map: map,
-		anchorPoint: new google.maps.Point(0, -29)
-	});
-
-	autocomplete.addListener('place_changed', function() {
-		infowindow.close();
-		marker.setVisible(false);
-		var place = autocomplete.getPlace();
-		if (!place.geometry) {
-			window.alert("Autocomplete's returned place contains no geometry");
-			return;
-		}
-
-		// If the place has a geometry, then present it on a map.
-		if (place.geometry.viewport) {
-			map.fitBounds(place.geometry.viewport);
-		} else {
-			map.setCenter(place.geometry.location);
-			map.setZoom(17);
-		}
-		marker.setIcon(({
-			url: place.icon,
-			size: new google.maps.Size(71, 71),
-			origin: new google.maps.Point(0, 0),
-			anchor: new google.maps.Point(17, 34),
-			scaledSize: new google.maps.Size(35, 35)
-		}));
-		marker.setPosition(place.geometry.location);
-		marker.setVisible(true);
-
-		var address = '';
-		if (place.address_components) {
-			address = [
-			(place.address_components[0] && place.address_components[0].short_name || ''),
-			(place.address_components[1] && place.address_components[1].short_name || ''),
-			(place.address_components[2] && place.address_components[2].short_name || '')
-			].join(' ');
-		}
-
-		infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-		infowindow.open(map, marker);
-	});
-
-	var myCenter = new google.maps.LatLng(1.5535, 110.3593);
-	var mapCanvas = document.getElementById("map");
-	var mapOptions = {center: myCenter, zoom: 13};
-	var map = new google.maps.Map(mapCanvas, mapOptions);
-	var marker = new google.maps.Marker({position:myCenter});
-	marker.setMap(map);
-}
 
 function formatPost(){
 	//format some stuff before posting to confirm.php
