f1c43268618907f9d23b6acaf53ec51080b0533e
Bryan Neo <bryanncy1999@gmail.com>
2018-05-23 10:16:12 +0800

    make footer font size smaller

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,6 @@
 		<button id="totopbutton" style="position:fixed;" class="w3-margin w3-display-bottomright w3-btn w3-round-large w3-theme-d3 w3-hide" onclick="window.scrollTo(0, 0);">Top</button>
 		<!-- Footer -->
-		<footer class="w3-container w3-theme-dark">
+		<footer class="w3-container w3-theme-dark" style="font-size:1.5vw;">
 			<p>&copy; Plan My Party 2018</p>
 			
 			<?php 
